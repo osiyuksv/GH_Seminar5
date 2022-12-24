8a1d141d05984f8a8f1888371008db8089faefc8
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-24 21:07:50 +0300

    Task4_arrNum10_99

---
+++ Task4_arrNum10_99/Program.cs
@@ -0,0 +1,15 @@
+﻿// в рандомном массиве выводить сколько 
+// элементов в диапазоне 10-99
+int[] arr0 = new int[12];
+int count = 0;
+
+  for (int i = 0; i < arr0.Length; i++)
+  {
+    arr0[i] = new Random().Next(1, 1000);
+
+    if((arr0[i] > 10) && (arr0[i] < 100)) count ++;
+  }
+
+Console.WriteLine(String.Join((", "), (arr0)));
+Console.WriteLine(count);
+
