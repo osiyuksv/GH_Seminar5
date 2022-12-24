45a6c3934c0a3370fd2a69dfc67beba426117aad
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-24 19:54:45 +0300

    Task2_Arr_minus_plus

---
+++ Task2_Arr_minus_plus/Program.cs
@@ -0,0 +1,15 @@
+﻿// заменить отрицательные элементы на положительные, и наоборот,
+// в рандомном массиве с 12 элементами в диапазоне [-9, 10]
+int[] arr0 = new int[4];
+
+for (int i = 0; i < arr0.Length; i++)
+  {
+    arr0[i] = new Random().Next(-9, 10); 
+  }
+Console.WriteLine(String.Join((", "), (arr0))); 
+
+for (int j = 0; j < arr0.Length; j++)
+  {
+    arr0[j] = -arr0[j];
+  }
+Console.WriteLine(String.Join((", "), (arr0)));
