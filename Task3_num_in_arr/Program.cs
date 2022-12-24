6e3a438c690ef52bb83160789ffafd1348e82c0c
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-24 20:23:18 +0300

    Task3_num_in_arr

---
+++ Task3_num_in_arr/Program.cs
@@ -0,0 +1,14 @@
+﻿// выводить есть ли заданное число в массиве
+int[] arr = {6, 7, 19, 345, 3};
+
+Console.WriteLine("введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] == num) sum ++;
+  }
+
+if(sum > 0) Console.WriteLine("да");
+else Console.WriteLine("нет");
